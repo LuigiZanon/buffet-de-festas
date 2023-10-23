7162c555e159022ddb0b44a0984d4c5b730e531f
SlendyGamer <gabrielcarotti@hotmail.com>
2023-10-23 00:47:24 -0300

    nav bar parcialmente funcional e site tomando forma final

--- resources/views/menu/menuP.blade.php
+++ resources/views/menu/menuP.blade.php
@@ -17,33 +17,21 @@
 <body>
     <img class="ImgLocal" src="https://media.discordapp.net/attachments/1138806499611779146/1164722422554038272/buufet.png?ex=65443f49&is=6531ca49&hm=164c4e7fc43f6beab5b9be910964de17074297b5911cd7a48580ac6d258b6d39&=" />
 
-    <div class="UI-TOPO">
-        <div class="ABA_TOP">
-            <p class="TitlePos">
-                <span class="Title_4">4</span> <span class="Title_Fun">fun Eventos &amp; Festas</span>
-            </p>
-            <div class="line1"></div>
-            <div class="line2"></div>
-
-                <img class="line-2" src="img/line-5.svg" />
-                <div class="ButtonPos">
-                    <div class="BT_AGENDA">
-                        <div class="text-wrapper-2">Agenda</div>
-                    </div>
-                </div>
-                <img class="line-3" src="img/line-6.svg" />
-                <div class="BT_CADASTRAR">
-                    <div class="Cadastrar">Cadastrar</div>
-                </div>
-                <div class="BT_ENTRAR">
-                    <div class="text-wrapper-4">Entrar</div>
-
-            </div>
-        </div>
+    <div class="navbar">
+        <img src="https://cdn.discordapp.com/attachments/1138806499611779146/1165802486444068885/4fun.png?ex=65482d2c&is=6535b82c&hm=488957ac2bdba0dc3565d36da0c09105294d5ac60a82b099cba50be54b18dc2c&" alt="4Fun Image" class="navbar-image">
+        <a href="#SobreNos"><span>Sobre nós</span></a>
+        <a href="#Contato"><span>Contato</span></a>
+        <a href="#"><span>Cadastrar</span></a>
+        <a href="#"><span>Entrar</span></a>
     </div>
 
-    <div class="SobreFix">
-        <div class="SobreBox"></div>
+        <p class="TitleSobImg">
+            <img src="https://cdn.discordapp.com/attachments/1138806499611779146/1165802486444068885/4fun.png?ex=65482d2c&is=6535b82c&hm=488957ac2bdba0dc3565d36da0c09105294d5ac60a82b099cba50be54b18dc2c&" alt="4Fun Image" class="LogoSobreIMG">
+        </p>
+
+
+    <div class="BoxSobre"  id="SobreNos">
+        <div class="SobreTitle">Sobre nós:</div>
         <p class="SobreTxt">
             O 4Fun Eventos &amp; Festas é o seu parceiro para momentos especiais. Com experiência e paixão por eventos,
             nossa equipe se dedica a tornar suas celebrações inesquecíveis. Da concepção à execução, cuidamos de cada
@@ -51,50 +39,43 @@
             o 4Fun Buffet é a escolha ideal. Deixe-nos fazer parte das suas memórias especiais e transformar seus sonhos
             em realidade. Bem-vindo à diversão no 4Fun Buffet!
         </p>
-        <div class="SobreTitle">Sobre nós:</div>
     </div>
 
-    <p class="TitleSobImg">
-        <span class="Title-4">4</span> <span class="Title-Fun">fun Buffet</span>
+    <img class="PLAYGROUND" src="https://media.discordapp.net/attachments/1138806499611779146/1164723152870457456/28278d4f561abab8d0106afe011e14a8.png?ex=65443ff7&is=6531caf7&hm=9a55093fd3cbbca7014c07e961a925f43ef1930e0dfcb03b5b74135a9c2b7635&=" />
+    <div class="DiversaoTitle">Diversão:</div>
+    <p class="DiversaoTxt">
+        No 4Fun Buffet, nossos pequenos convidados têm um espaço especial para brincar e se divertir. Nosso playground
+        infantil é projetado com segurança e diversão em mente, proporcionando às crianças um ambiente empolgante para
+        explorar enquanto os adultos desfrutam das celebrações.
     </p>
-    <div class="DiversaoFix">
-        <div class="DiversaoTitle">Diversão:</div>
-        <p class="DiversaoTxt">
-            No 4Fun Buffet, nossos pequenos convidados têm um espaço especial para brincar e se divertir. Nosso playground
-            infantil é projetado com segurança e diversão em mente, proporcionando às crianças um ambiente empolgante para
-            explorar enquanto os adultos desfrutam das celebrações.
-        </p>
-    </div>
 
     <img class="line-4" src="img/line-7.svg" />
 
     <img class="line-5" src="img/line-8.svg" />
 
-    <img class="PLAYGROUND" src="https://media.discordapp.net/attachments/1138806499611779146/1164723152870457456/28278d4f561abab8d0106afe011e14a8.png?ex=65443ff7&is=6531caf7&hm=9a55093fd3cbbca7014c07e961a925f43ef1930e0dfcb03b5b74135a9c2b7635&=" />
 
     <img class="COMIDA" src="https://media.discordapp.net/attachments/1138806499611779146/1164723126664450148/94a893ff470079472f7a63e640a488d3.png?ex=65443ff0&is=6531caf0&hm=f6675fcb91a265963d614133b91670430d8ac8ecd97e1e94ffa9800c1a90a3f0&=" />
-
-    <div class="MenuFix">
-        <div class="MenuTitle">Menu:</div>
-        <p class="MenuTxt">
-            Em nosso cardápio, oferecemos diversos pacotes de alimentação completos a preços variados. Nosso objetivo é
-            proporcionar opções que atendam a diversas preferências e orçamentos, desde pratos gourmet até escolhas mais
-            descontraídas. No 4Fun Buffet, a diversidade está no cardápio, e estamos aqui para tornar cada refeição uma
-            deliciosa parte de suas celebrações.
-        </p>
-    </div>
+    <div class="MenuTitle">Menu:</div>
+    <p class="MenuTxt">
+        Em nosso cardápio, oferecemos diversos pacotes de alimentação completos a preços variados. Nosso objetivo é
+        proporcionar opções que atendam a diversas preferências e orçamentos, desde pratos gourmet até escolhas mais
+        descontraídas. No 4Fun Buffet, a diversidade está no cardápio, e estamos aqui para tornar cada refeição uma
+        deliciosa parte de suas celebrações.
+    </p>
 
     <div class="ContatoFix">
-        <div class="Contato">Contato:</div>
-        <div class="Tel">Telefone:</div>
-        <div class="TelNum">(19) 911</div>
-        <img class="TelIcon" src="https://media.discordapp.net/attachments/1138806499611779146/1164742305295646780/icons8-telephone-30.png?ex=654451cd&is=6531dccd&hm=930265dcfeeb20a36440954c240fa276a239c025ef337e7cc588eba40c055754&=" />
-        <img class="TelIcon2" src="https://media.discordapp.net/attachments/1138806499611779146/1164742305295646780/icons8-telephone-30.png?ex=654451cd&is=6531dccd&hm=930265dcfeeb20a36440954c240fa276a239c025ef337e7cc588eba40c055754&=" />
-        <div class="TelNum2">(19) 190</div>
-        <div class="Email">Email:</div>
-        <div class="Mail">4fun-festas@email.com.br</div>
-        <div class="Endereco">Endereço:</div>
-        <p class="End">Avenida Jacinto Pinto Gomes, 872, São Paulo, Campinas</p>
+        <div id="Contato">
+            <div class="Contato">Contato:</div>
+            <div class="Tel">Telefone:</div>
+            <div class="TelNum">(19) 911</div>
+            <img class="TelIcon" src="https://media.discordapp.net/attachments/1138806499611779146/1164742305295646780/icons8-telephone-30.png?ex=654451cd&is=6531dccd&hm=930265dcfeeb20a36440954c240fa276a239c025ef337e7cc588eba40c055754&=" />
+            <img class="TelIcon2" src="https://media.discordapp.net/attachments/1138806499611779146/1164742305295646780/icons8-telephone-30.png?ex=654451cd&is=6531dccd&hm=930265dcfeeb20a36440954c240fa276a239c025ef337e7cc588eba40c055754&=" />
+            <div class="TelNum2">(19) 190</div>
+            <div class="Email">Email:</div>
+            <div class="Mail">4fun-festas@email.com.br</div>
+            <div class="Endereco">Endereço:</div>
+            <p class="End">Avenida Jacinto Pinto Gomes, 872, São Paulo, Campinas</p>
+        </div>
     </div>
 </body>
 
