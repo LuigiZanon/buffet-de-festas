fb6ba91b8a8b43cf4d98e54c4b2497f4a0a4f564
LuigiZanon <lugazabugre@outlook.com>
2023-11-10 14:05:48 -0300

    access_admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,7 +23,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('access', function (User $user){
+        Gate::define('access_admin', function (User $user){
             return $user->access_level == 'admin';
     });
 }
