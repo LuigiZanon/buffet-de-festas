0572521600a9c7ba12a7fef5ecf60aaa798a873f
arnaldoflorenc <142014114+arnaldoflorenc@users.noreply.github.com>
2023-11-10 14:59:52 -0300

    access_admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,9 +10,10 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
-                    @can('access')
+                    @can('access_admin')
                     <p>Olá {{Auth::user() -> name}}</p>
                     @endcan
+
 
                     {{ __("You're logged in!") }}
                 </div>
