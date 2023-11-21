13be4faef3c44a9ea781a6dd6a2cbf23b8061233
SlendyGamer <gabrielcarotti@hotmail.com>
2023-11-20 21:23:43 -0300

    exemplo de permissionamento

--- resources/views/AGENDA/MENUreserva.blade.php
+++ resources/views/AGENDA/MENUreserva.blade.php
@@ -4,12 +4,34 @@
             {{ __('Agendamentos') }}
         </h2>
     </x-slot>
-
+    @php
+    $user = Auth::user();
+    $allowedPermissions = ['access_comercial', 'access_admin'];
+    @endphp
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="flex flex-col items-center space-y-4">
+                        @if (collect($allowedPermissions)->contains(function ($permission) use ($user) {
+                          return $user->can($permission);
+                      }))
+                        <a href="{{ route('AGENDA.ADMreserva') }}"
+                        class="rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        data-ripple-light="true">
+                         GERENCIAR RESERVAS
+                     </a>
+                     <a href="{{ route('HORAfuncionamento') }}"
+                     class="rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                     data-ripple-light="true">
+                      HORARIO DE FUNCIONAMENTO
+                  </a>
+                  <a href="{{ route('edit.rec') }}"
+                  class="rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  data-ripple-light="true">
+                  EDITAR RECOMENDAÇÕES
+              </a>
+              @else
                         <a href="{{ route('AGENDA.CRIAreserva')}}"
                         class="rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                         data-ripple-light="true">
@@ -21,23 +43,7 @@
                      data-ripple-light="true">
                       VERIFICAR STATUS
                   </a>
-
-                  <a href="{{ route('AGENDA.ADMreserva') }}"
-                  class="rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  data-ripple-light="true">
-                   GERENCIAR RESERVAS
-               </a>
-               <a href="{{ route('HORAfuncionamento') }}"
-               class="rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-               data-ripple-light="true">
-                HORARIO DE FUNCIONAMENTO
-            </a>
-            <a href="{{ route('edit.rec') }}"
-            class="rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            data-ripple-light="true">
-             EDITAR RECOMENDAÇÕES
-         </a>
-
+                  @endif
                     </div>
 
                       <!-- Ripple Effect from cdn -->
